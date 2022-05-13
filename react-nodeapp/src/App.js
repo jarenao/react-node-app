4e6da8ceda58a14353d9a5b8176b2e17615f1b56
Basilio <basilio.uvinas@gmail.com>
2022-05-13 19:48:03 +0200

    Feat: Configuración inicial - Create App

---
+++ react-nodeapp/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">hola que tal</div>;
+}
+
+export default App;
---
+++ react-nodeapp/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./css/bootstrap.min.css";
+import "./css/fontAwesome.css";
+import "./css/templatemo-style.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
