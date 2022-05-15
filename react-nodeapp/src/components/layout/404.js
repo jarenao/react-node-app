2faad055f19fc7c0635e92ce28347fa55683c886
Basilio <basilio.uvinas@gmail.com>
2022-05-15 20:17:18 +0200

    Feat: Crear componente 404 / NotFound

---
+++ react-nodeapp/src/components/layout/404.js
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <div className="error-container">
+      <h1>404</h1>
+      <p className="return">
+        Back to <a href="#">Home page</a>
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
