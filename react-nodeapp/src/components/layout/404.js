9647abac152f58e55ffe680926abcda85b1317c7
Basilio <basilio.uvinas@gmail.com>
2022-05-15 20:23:57 +0200

    Fix: ClassName 404 icon

--- react-nodeapp/src/components/layout/404.js
+++ react-nodeapp/src/components/layout/404.js
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <div className="error-container">
       <h1>
-        4<i class="fa fa-frown-o" aria-hidden="true"></i>4
+        4<i className="fa fa-frown-o"></i>4
       </h1>
       <p className="return">
         Back to <a href="#">Home page</a>
