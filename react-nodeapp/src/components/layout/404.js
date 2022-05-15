4fa16485e7f6f3405c3479d27b16bb946fa4993f
Basilio <basilio.uvinas@gmail.com>
2022-05-15 20:20:47 +0200

    Setting: Icono en 404 page

--- react-nodeapp/src/components/layout/404.js
+++ react-nodeapp/src/components/layout/404.js
@@ -1,7 +1,9 @@
 const NotFound = () => {
   return (
     <div className="error-container">
-      <h1>404</h1>
+      <h1>
+        4<i class="fa fa-frown-o" aria-hidden="true"></i>4
+      </h1>
       <p className="return">
         Back to <a href="#">Home page</a>
       </p>
